386911d8a27b72798e632f4d48e7a85a435b2d2e
danilaple <99321805+danilaple@users.noreply.github.com>
2022-06-08 18:35:08 +0300

    Update индив.2.py

--- индив.2.py
+++ индив.2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-text = input("Введите текст:")
-print(text.replace('ах', 'ух'))
+if __name__ == '__main__':
+  text = input("Введите текст:")
+  print(text.replace('ах', 'ух'))
